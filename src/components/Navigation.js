a8645913d2e1794c81608221ae13f38264e30e7a
EASY_ARAV <wg0418@gmail.com>
2020-09-30 23:27:45 +0900

    create gitblog project page

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navigation.css";
+
+function Navigation() {
+  return (
+    <div className="nav">
+      <a href="https://arav-easy1.github.io/">Home</a>
+      <a href="https://arav-easy1.github.io/study">Study</a>
+      <a href="https://arav-easy1.github.io/project">Project</a>
+    </div>
+  );
+}
+
+export default Navigation;
---
+++ src/routes/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <p>프로젝트들 올라갈 것임.</p>
+    </div>
+  );
+}
+
+export default Home;
